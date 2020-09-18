65756105cbe6b23c06cef7dcb8d530b7f8f5df2d
StudioDivier <ianfoxstudio@gmail.com>
2020-09-18 17:43:22 +0300

    end-point Для всех вьюшек + добавлены настройки с отгрузкой медиа

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import include, path, re_path
 from rest_framework import routers
 from .views import (
-CustomerCreate, StarCreate, StarsList, StarsViewSet, RateStar, StarByCategory, TestView)
+CustomerCreate, StarCreate, StarsList, StarsViewSet, RateStar, StarByCategory, TestView, OrderView)
+
+from MyStar import settings
+from django.conf import settings
+from django.conf.urls.static import static
 
 router = routers.DefaultRouter()
 router.register(r'stars', StarsViewSet)
-
 
 urlpatterns = [
     path('', include(router.urls)),
@@ -15,7 +18,10 @@ urlpatterns = [
     path('star/getlist/', StarsList.as_view(), name=None),
     path('ratestar', RateStar.as_view(), name=None),
     path('starlist/category', StarByCategory.as_view(), name=None),
+    path('order', OrderView.as_view(), name=None),
 
     path('test', TestView.as_view(), name=None)
-
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
